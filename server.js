4e109737bf48a1c6c8be4357d4016a84eca90b5c
Your Name <you@example.com>
2026-07-06 18:32:35 +0530

    commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
 })
 
 app.use(cors({
-    origin: "https://recipe-list-chi.vercel.app",
+    origin: "https://recipe-list-9utf.vercel.app",
     credentials: true
 }));
 app.use(express.json())
